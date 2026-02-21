5dcca4334db399846376fa5fdc95e4208052e70e
RotesBlatt <blauerhut04@gmail.com>
2026-02-21 12:31:52 +0100

    feat: update Gradle setup and Android SDK configuration in build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,36 +3,43 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     kotlin("multiplatform") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
+    id("com.android.library") version "8.1.4"
     id("org.openapi.generator") version "7.3.0"
     id("maven-publish")
     id("signing")
 }
 
 group = "io.github.rotesblatt"
+
 version = "1.6.0"
 
 repositories {
     mavenCentral()
+    google()
+}
+
+android {
+    namespace = "io.github.rotesblatt.hextractor"
+    compileSdk = 36
+
+    defaultConfig { minSdk = 24 }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            compilerOptions.configure {
-                jvmTarget.set(JvmTarget.JVM_17)
-            }
-        }
+    androidTarget {
+        compilations.all { compilerOptions.configure { jvmTarget.set(JvmTarget.JVM_17) } }
+        publishLibraryVariants("release")
     }
-    
+
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-    
-    androidNativeArm32()
-    androidNativeArm64()
-    androidNativeX86()
-    androidNativeX64()
-    
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -42,24 +49,11 @@ kotlin {
                 implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
             }
         }
-        
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        
-        val jvmMain by getting
-        val jvmTest by getting
-        
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+
+        val commonTest by getting { dependencies { implementation(kotlin("test")) } }
+
+        val androidMain by getting {
+            dependencies { implementation("io.ktor:ktor-client-android:2.3.7") }
         }
     }
 }
@@ -72,35 +66,21 @@ openApiGenerate {
     apiPackage.set("com.hextractor.api")
     modelPackage.set("com.hextractor.api.model")
     invokerPackage.set("com.hextractor.api.client")
-    
-    configOptions.set(mapOf(
-        "library" to "multiplatform",
-        "dateLibrary" to "kotlinx-datetime",
-        "useCoroutines" to "true",
-        "enumPropertyNaming" to "UPPERCASE",
-        "collectionType" to "list"
-    ))
+
+    configOptions.set(
+            mapOf(
+                    "library" to "multiplatform",
+                    "dateLibrary" to "kotlinx-datetime",
+                    "useCoroutines" to "true",
+                    "enumPropertyNaming" to "UPPERCASE",
+                    "collectionType" to "list"
+            )
+    )
 }
 
 // Add generated sources to Kotlin source sets
 kotlin.sourceSets.getByName("commonMain") {
     kotlin.srcDir("$buildDir/generated/src/commonMain/kotlin")
-}
-
-tasks.named("compileKotlinJvm") {
-    dependsOn("openApiGenerate")
-}
-
-tasks.named("compileKotlinIosX64") {
-    dependsOn("openApiGenerate")
-}
-
-tasks.named("compileKotlinIosArm64") {
-    dependsOn("openApiGenerate")
-}
-
-tasks.named("compileKotlinIosSimulatorArm64") {
-    dependsOn("openApiGenerate")
 }
 
 // Publishing configuration
@@ -111,14 +91,14 @@ publishing {
                 name.set("Hex-Tractor API Client")
                 description.set("Generated Kotlin Multiplatform client for Hex-Tractor API")
                 url.set("https://github.com/RotesBlatt/hex-specs")
-                
+
                 licenses {
                     license {
                         name.set("MIT License")
                         url.set("https://opensource.org/licenses/MIT")
                     }
                 }
-                
+
                 developers {
                     developer {
                         id.set("RotesBlatt")
@@ -126,7 +106,7 @@ publishing {
                         email.set("blauerhut04@gmail.com")
                     }
                 }
-                
+
                 scm {
                     connection.set("scm:git:git://github.com/RotesBlatt/hex-specs.git")
                     developerConnection.set("scm:git:ssh://github.com/RotesBlatt/hex-specs.git")
@@ -135,11 +115,14 @@ publishing {
             }
         }
     }
-    
+
     repositories {
         maven {
             name = "CentralPortal"
-            url = uri("https://central.sonatype.com/api/v1/publisher/upload?publishingType=USER_MANAGED")
+            url =
+                    uri(
+                            "https://central.sonatype.com/api/v1/publisher/upload?publishingType=USER_MANAGED"
+                    )
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
                 password = System.getenv("MAVEN_PASSWORD")
@@ -149,14 +132,9 @@ publishing {
 }
 
 signing {
-    useInMemoryPgpKeys(
-        System.getenv("GPG_PRIVATE_KEY"),
-        System.getenv("GPG_PASSPHRASE")
-    )
+    useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSPHRASE"))
     sign(publishing.publications)
 }
 
 // Only sign if publishing
-tasks.withType<Sign>().configureEach {
-    onlyIf { System.getenv("GPG_PRIVATE_KEY") != null }
-}
+tasks.withType<Sign>().configureEach { onlyIf { System.getenv("GPG_PRIVATE_KEY") != null } }
